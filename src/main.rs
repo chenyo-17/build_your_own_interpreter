bf3ca55739149a67c96ee3a3ca7f52041b29edd7
chenyo-17 <cy970802@hotmail.com>
2025-01-13 13:23:52 +0100

    UE7: string literal

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use derive_more::Display;
 use either::*;
 use std::env;
 use std::fs::read_to_string;
-use std::path::{Path, PathBuf};
 use thiserror::Error;
 
 #[derive(Display)]
@@ -46,12 +45,13 @@ enum TokenType {
     GreaterEqual,
     #[display("SLASH")]
     Slash,
+    #[display("STRING")]
+    String_,
     #[display("EOF")]
     Eof,
 }
 
 impl TokenType {
-    // TODO: whether using a hashmap to store the lexeme and its name is better?
     fn scan_single_char_token(content: &str) -> Option<Self> {
         if content.is_empty() {
             return None;
@@ -89,29 +89,72 @@ impl TokenType {
         }
     }
 
-    /// Greedy match on the longest token, return the matched token type
-    /// and the token length
-    fn scan_token(content: &str) -> Option<(Self, usize)> {
-        if let Some(token_type) = Self::scan_double_char_token(content) {
-            return Some((token_type, 2));
-        } else if let Some(token_type) = Self::scan_single_char_token(content) {
-            return Some((token_type, 1));
+    /// If no left `"` is found, returns `Ok(None)`;
+    /// If a pair of `"` is found, returns the string literal length (both " are exclusived);
+    /// If the right `"` is not found, return the error.
+    /// The current line is updated accordingly when a newline is found in the string literal.
+    fn scan_string_literal(content: &str, line: &mut usize) -> anyhow::Result<Option<usize>> {
+        if content.is_empty() || &content[..1] != "\"" {
+            return Ok(None);
         }
-        None
+        for (i, c) in content[1..].char_indices() {
+            if c == '\n' {
+                *line += 1;
+            }
+            if c == '"' {
+                return Ok(Some(i)); // " exclusive
+            }
+        }
+        Err(ScanError::UnterminedString(*line).into())
+    }
+
+    /// Greedy match on the longest token, construct and return the `Token`
+    /// if no token is matched, return `Ok(None)`,
+    /// if an error is encountered, propagate it to the caller.
+    /// `line` and `offset` are updated during the scan.
+    fn scan_token<'a>(
+        content: &'a str,
+        line: &mut usize,
+        offset: &mut usize,
+    ) -> anyhow::Result<Option<Token<'a>>> {
+        match Self::scan_string_literal(content, line) {
+            Err(e) => {
+                *offset += content.len(); // has scanned the entire file
+                return Err(e);
+            }
+            Ok(Some(len)) => {
+                *offset += len + 2; // include ""
+                return Ok(Some(Token::new(
+                    Self::String_,
+                    Some(&content[..len + 2]),                // include ""
+                    Some(Either::Left(&content[1..len + 1])), // exclude "
+                )));
+            }
+            Ok(None) => {
+                if let Some(token_type) = Self::scan_double_char_token(content) {
+                    *offset += 2;
+                    return Ok(Some(Token::new(token_type, Some(&content[..2]), None)));
+                } else if let Some(token_type) = Self::scan_single_char_token(content) {
+                    *offset += 1;
+                    return Ok(Some(Token::new(token_type, Some(&content[..1]), None)));
+                }
+            }
+        }
+        Ok(None)
     }
 }
 
-struct Token {
+struct Token<'a> {
     token_type: TokenType,
-    lexeme: Option<String>,
-    literal: Option<Either<String, i32>>,
+    lexeme: Option<&'a str>,
+    literal: Option<Either<&'a str, i32>>,
 }
 
-impl Token {
+impl<'a> Token<'a> {
     fn new(
         token_type: TokenType,
-        lexeme: Option<String>,
-        literal: Option<Either<String, i32>>,
+        lexeme: Option<&'a str>,
+        literal: Option<Either<&'a str, i32>>,
     ) -> Self {
         Self {
             token_type,
@@ -121,7 +164,7 @@ impl Token {
     }
 }
 
-impl Default for Token {
+impl Default for Token<'_> {
     fn default() -> Self {
         Self {
             token_type: TokenType::Eof,
@@ -131,16 +174,14 @@ impl Default for Token {
     }
 }
 
-impl fmt::Display for Token {
+impl fmt::Display for Token<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
             "{} {} {}",
             self.token_type,
-            self.lexeme.as_ref().unwrap_or(&String::new()),
-            self.literal
-                .as_ref()
-                .unwrap_or(&Either::Left("null".to_string()))
+            self.lexeme.as_ref().unwrap_or(&""),
+            self.literal.as_ref().unwrap_or(&Either::Left("null"))
         )
     }
 }
@@ -149,22 +190,15 @@ impl fmt::Display for Token {
 enum ScanError {
     #[error("[line {1}] Error: Unexpected character: {0}")]
     UnexpectedChar(char, usize),
+    #[error("[line {0}] Error: Unterminated string.")]
+    UnterminedString(usize),
 }
 
-struct Scanner {
-    path: PathBuf,
-    tokens: Vec<Token>,
-}
+#[derive(Default)]
+struct Scanner<'a>(Vec<Token<'a>>);
 
-impl Scanner {
-    fn new(path: impl AsRef<Path>) -> Self {
-        Self {
-            path: path.as_ref().into(),
-            tokens: vec![],
-        }
-    }
-
-    fn scan_tokens(&mut self) -> anyhow::Result<()> {
+impl<'a> Scanner<'a> {
+    fn scan_tokens(&mut self, source: &'a str) -> anyhow::Result<()> {
         fn is_comment(content: &str) -> bool {
             if content.len() < 2 {
                 return false;
@@ -176,20 +210,8 @@ impl Scanner {
         }
 
         fn is_whitespace(c: char) -> bool {
-            match c {
-                ' ' | '\r' | '\t' => true,
-                _ => false,
-            }
+            matches!(c, ' ' | '\r' | '\t')
         }
-
-        fn is_newline(c: char) -> bool {
-            c == '\n'
-        }
-
-        let Ok(source) = read_to_string(&self.path) else {
-            self.tokens.push(Token::default());
-            return Ok(());
-        };
 
         let mut result = Ok(());
         let (mut line, mut offset) = (1, 0);
@@ -197,23 +219,22 @@ impl Scanner {
             if is_comment(&source[offset..]) {
                 // skip the whole line
                 offset = source[offset..]
-                    .find(is_newline)
+                    .find('\n')
                     .map(|p| offset + p)
                     .unwrap_or(source.len());
                 continue;
             }
-            match TokenType::scan_token(&source[offset..]) {
-                Some((token_type, token_length)) => {
-                    self.tokens.push(Token::new(
-                        token_type,
-                        Some(String::from(&source[offset..offset + token_length])),
-                        None,
-                    ));
-                    offset += token_length;
+            match TokenType::scan_token(&source[offset..], &mut line, &mut offset) {
+                Err(e) => {
+                    eprintln!("{}", e);
+                    result = Err(e);
                 }
-                None => {
+                Ok(Some(token)) => {
+                    self.0.push(token);
+                }
+                Ok(None) => {
                     let c = source.chars().nth(offset).unwrap();
-                    if is_newline(c) {
+                    if c == '\n' {
                         line += 1;
                     } else if !is_whitespace(c) {
                         eprintln!("{}", ScanError::UnexpectedChar(c, line));
@@ -223,7 +244,7 @@ impl Scanner {
                 }
             }
         }
-        self.tokens.push(Token::new(TokenType::Eof, None, None));
+        self.0.push(Token::default());
         result
     }
 }
@@ -238,12 +259,18 @@ fn main() {
     let command = &args[1];
     let filename = &args[2];
 
+    let Ok(source) = read_to_string(filename) else {
+        // file not found
+        println!("{}", Token::default());
+        return;
+    };
+
     match command.as_str() {
         "tokenize" => {
-            let mut scanner = Scanner::new(filename);
+            let mut scanner = Scanner::default();
 
-            let result = scanner.scan_tokens();
-            for token in &scanner.tokens {
+            let result = scanner.scan_tokens(&source);
+            for token in &scanner.0 {
                 println!("{token}");
             }
             if result.is_err() {
